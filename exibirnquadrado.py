f83b1ca5c339a9978c64ba1c90f9c302967159c5
Bessean <55710829+Bessean@users.noreply.github.com>
2021-04-29 22:55:48 -0300

    add

---
+++ exibirnquadrado.py
@@ -0,0 +1,7 @@
+#Exibir a soma entre 3 números ao quadrado
+
+n1=int(input("Digite a n1: "))
+n2=int(input("Digite a n2: "))
+n3=int(input("Digite a n3: "))
+soma = n1**2 + n2**2 + n3**2
+print (" A soma do quadrado entre os números é: ", soma)
